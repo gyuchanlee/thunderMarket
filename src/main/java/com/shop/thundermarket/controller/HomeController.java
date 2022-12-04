7d690259e9c8568fbf1ae3de8c2d0e4479d3018f
gyuchanlee <hoo788@gmail.com>
2022-12-04 23:46:59 +0900

    spring security config 기초 구성 및 테스팅

--- src/main/java/com/shop/thundermarket/controller/HomeController.java
+++ src/main/java/com/shop/thundermarket/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.shop.thundermarket.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        log.info("home controller");
         return "home";
+    }
+
+    @GetMapping("/test/test1")
+    public String test() {
+        log.info("test controller");
+        return "test/test";
     }
 
 }
